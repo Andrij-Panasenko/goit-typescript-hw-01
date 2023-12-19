3954ac0e567949df4625b3b962e589e9e9cd6af4
a.panasenko <andrii.panasenko295@gmail.com>
2023-12-19 17:17:40 +0200

    ignore

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,14 @@
-// явне визначення типів
-let tod = 100;
-let userName = "bob";
-let isActive = false;
-let empty = null;
-let undf = undefined;
-// неявне визначення типів
-let age = 10;
-age = 12;
-//визначення типів в складних типах данних
-const numbers = [1, 2, 3, 4, 5, 6];
-numbers.push(12);
+// type User = {
+//    name: string;
+//    age: number | string,
+// };
+const admin = {
+    name: "jou",
+    age: 12,
+    role: "admin"
+};
 const user = {
-    name: "bob",
-    age: 10,
+    name: 'lolo',
+    age: 44,
 };
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,108 @@
-// явне визначення типів
-let tod: number = 100;
+// type User = {
+//    name: string;
+//    age: number | string,
+// };
 
-let userName: string = "bob";
-let isActive: boolean = false;
-let empty: null = null;
-let undf: undefined = undefined;
+// явне визначення типів
+// let tod: number = 100;
+
+// let userName: string = "bob";
+// let isActive: boolean = false;
+// let empty: null = null;
+// let undf: undefined = undefined;
 
 // неявне визначення типів
-let age = 10;
+// let age = 10;
 
-age = 12
-
+// age = 12;
 
 //визначення типів в складних типах данних
-const numbers: number[] = [1, 2, 3, 4, 5, 6];
+// const numbers: number[] = [1, 2, 3, 4, 5, 6];
 
-numbers.push(12)
+// numbers.push(12);
 
-type User = { name: string, age: number }
+// const user: User = {
+//   name: "bob",
+//   age: 10,
+// };
+
+
+// type size = "smal" | "medium" | "large";
+// enum Sizes {
+//    small = "small",
+//    medium = "medium",
+//    large="large",
+// }
+// const button: size = "large";
+// const buttonA: Sizes = Sizes.large;
+
+
+//Типізація функцій                      //типізація повернення з ф-ція
+// function add(num1: number, num2: number): string {
+//    return `${num1} + ${num2}`;
+// }
+
+// console.log(add(2, 3));
+
+                                         //без return
+// function add(num1: number, num2: number): void {
+//    console.log(num1 + num2);
+// }
+
+// add(23,32)
+
+// type User = {
+//    name: string;
+// }
+// function getHello(user: User): void {
+//    console.log('Hello', user.name)
+// }
+
+
+//тип return(-y)
+// type User = {
+//    name: string,
+//    age: number,
+//    hobbie:string,
+// }
+// function userConstructor(name: string, age: number, hobbie: string): User {
+//    return {
+//       name, age, hobbie,
+//    }
+// }
+
+
+
+
+// type Car = {
+//    color: string,
+//    price: number,
+//    currency: string,
+//    start: () => {}
+// }
+// const Car = {
+//    color: "red",
+//    price: 1000,
+//    currency: "UAH",
+//    start() {
+//       console.log("Start")
+//    }
+// }
+
+type User = { 
+   name: string;
+   age: number;
+   role?: string;
+}
+
+const admin: User = {
+   name: "jou",
+   age: 12,
+   role: "admin"
+}
+
 const user: User = {
-   name: "bob",
-   age: 10,
+   name: 'lolo',
+   age: 44,
+   
 }
